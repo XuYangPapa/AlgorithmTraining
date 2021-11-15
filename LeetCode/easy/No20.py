47c5591fddbfec21d0e02c1b2c6647fc01cc614d
yangxu <1056140155@qq.com>
2021-11-15 22:24:19 +0800

    <No20>:有效的括号

---
+++ LeetCode/easy/No20.py
@@ -0,0 +1,23 @@
+class No20:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 == 1:
+            return False
+        reference_dict = {'(': ')', '[': ']', '{': '}'}
+        stack = []
+        for char in s:
+            if char in reference_dict.keys():
+                stack.append(char)
+            else:
+                if len(stack) == 0:
+                    return False
+                left = stack.pop()
+                if reference_dict.get(left) == char:
+                    continue
+                else:
+                    return False
+        return not stack
+
+
+if __name__ == '__main__':
+    no20 = No20()
+    print(no20.isValid('{}[]()[(]]'))
