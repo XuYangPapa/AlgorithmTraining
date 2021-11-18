ca80ef1c0e57e9b27eacc2b58e5a25be3c2f6550
XuYangPapa <1056140155@qq.com>
2021-11-18 21:28:31 +0800

    <No27>:移除元素

---
+++ LeetCode/easy/No27.py
@@ -0,0 +1,29 @@
+"""
+27.移除元素
+"""
+from typing import List
+
+
+class No27:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        """
+        前后双指针遍历
+        """
+        if len(nums) == 0:
+            return 0
+        save, remove = 0, len(nums) - 1
+        while save <= remove:
+            if nums[save] == val:
+                nums[save] = nums[remove]
+                remove -= 1
+            else:
+                save += 1
+        return remove + 1 if remove >= 0 else 0
+
+
+if __name__ == '__main__':
+    no27 = No27()
+    nums = [0, 1, 2, 2, 3, 0, 4, 2]
+    size = no27.removeElement(nums, 2)
+    for i in range(size):
+        print(nums[i])
